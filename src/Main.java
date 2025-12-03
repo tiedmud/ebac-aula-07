844c344aae9f9ea3a4d3349eb829f1f52216457d
Ricardo Takara <ricardo.takara.1830@live.com>
2025-12-03 04:45:26 -0300

    Completo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,6 +8,23 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Aula 03");
 
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int inteiro1 = scanner.nextInt();
+
+        System.out.print("Digite outro número inteiro: ");
+        int inteiro2 = scanner.nextInt();
+
+        scanner.close();
+
+        if (inteiro1 > inteiro2) {
+            System.out.println("O maior número é " + inteiro1);
+        } else if  (inteiro1 < inteiro2) {
+            System.out.println("O maior número é " + inteiro2);
+        } else {
+            System.out.println("Os números são iguais!");
+        }
+
     }
 }
